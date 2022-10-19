40185258337ca88981844cc163009f9b2b8878ff
dev.anhdq <dev.anhdq@gmail.com>
2022-10-19 10:40:38 +0700

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Col, Container, ListGroup, ListGroupItem, Row} from "reactstrap";
+
+import "./footer.css"
 import logo from "../../assets/images/res-logo.png";
 
 const Footer = () => {
@@ -10,24 +12,23 @@ const Footer = () => {
                     <Col lg='3' md='4' sm='6'>
                         <div className="logo footer__logo text-start">
                             <img src={logo} alt="Logo..."/>
-                            <h5>Tasty Treat</h5>
+                            <h5 className="footer__title">Tasty Treat</h5>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus eos iste maiores quasi
                                 veritatis. Dolorum laborum magni quisquam repudiandae velit.</p>
                         </div>
                     </Col>
                     <Col lg='3' md='4' sm='6'>
                         <h5>Delivery Time</h5>
-                        <ListGroup>
-
-                            <ListGroupItem className="border-0 ps-0">
+                        <ListGroup className="delivery__time-list">
+                            <ListGroupItem className="delivery__time-item border-0 ps-0">
                                 <span> Sunday - Thursday</span>
                                 <p>10:00 - 11:00pm</p>
                             </ListGroupItem>
-                            <ListGroupItem className="border-0 ps-0">
+
+                            <ListGroupItem className="delivery__time-item border-0 ps-0">
                                 <span> Friday - Saturday</span>
                                 <p>Off day</p>
                             </ListGroupItem>
-
                         </ListGroup>
                     </Col>
                     <Col lg='3' md='4' sm='6'>
